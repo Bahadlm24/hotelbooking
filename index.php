bd48b7d4af6bf6c14c4be7c55b78bd4ac265d05c
Bahadlm24 <bahadirperveli01@gmail.com>
2024-02-19 18:18:06 +0300

    title eklendi

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link  rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
   <?php require('inc/links.php'); ?>
-  <title><?php echo $settings_r['site_title'] ?> - Pansiyon</title>
+  <title><?php echo $settings_r['site_title'] ?> - Gürcan Pansiyon</title>
   <style>
     .availability-form{
       margin-top: -50px;
